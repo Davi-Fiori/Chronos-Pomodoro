e73018ace99c49f2599804e07da048217ba9d5d6
Davi Fiori <05725@gruposooro.com.br>
2025-05-07 17:16:07 -0300

    novo5

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+
+import "./styles/global.css";
+import "./styles/theme.css";
+
+export function App() {
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+    </>
+  );
+}
+//Modificado 17:00
